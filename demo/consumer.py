583681e877b7cac32e76909402cf5c225ffe5d04
John Brandborg <jbr@trackunit.com>
2022-05-29 19:38:46 +0200

    Make consumer run on as a main module

--- demo/consumer.py
+++ demo/consumer.py
@@ -2,9 +2,20 @@
 Kafka Consumer to Console
 """
 
+import sys
+
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('test', bootstrap_servers='kafka', group_id='c1')
 
-for msg in consumer:
-    print(msg)
+def main():
+    consumer = KafkaConsumer('test', bootstrap_servers='kafka', group_id='c1')
+
+    for msg in consumer:
+        print(msg)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt as error:
+        print("\nExiting.", error, file=sys.stderr)
