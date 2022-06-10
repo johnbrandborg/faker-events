f6cf752c5f88a3c7cb56c866bdafd3848b1dd22d
John Brandborg <jbr@trackunit.com>
2022-06-10 23:39:56 +0200

    Update CLI Testing

--- faker_events/__main__.py
+++ faker_events/__main__.py
@@ -4,14 +4,14 @@ Main module for Faker-Events
 Runs the Example EventType
 """
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 import importlib
 import os
 import sys
 
-from faker_events import EventGenerator
-from faker_events import ProfileGenerator
+from faker_events import EventGenerator, ProfileGenerator
 from faker_events.text_color import eprint, Palatte
+
 
 parser = ArgumentParser(prog="python -m faker_events",
                         description="Faker Events creates JSON events, and"
@@ -35,30 +35,36 @@ parser.add_argument("-u", "--update",
                     action='store_true',
                     dest="update_profiles",
                     help="Save profile data on completion")
-args = parser.parse_args()
 
-profiles_generator = ProfileGenerator()
-profiles_generator.load(args.num_profiles, args.profiles_file)
-event_generator = EventGenerator(profiles_generator)
 
-if args.script:
+def cli(args: Namespace):
+    profiles_generator = ProfileGenerator()
+    profiles_generator.load(args.num_profiles, args.profiles_file)
+    event_generator = EventGenerator(profiles_generator)
+
+    if args.script:
+        try:
+            sys.path.append(os.getcwd())
+            module_path = os.path.splitext(args.script)[0].replace("/", ".")
+            importlib.import_module(module_path)
+        except ModuleNotFoundError:
+            eprint(f"ERROR: No event module named '{args.script}'",
+                   Palatte.RED)
+            return 1
+    else:
+        import faker_events.example  # noqa
+
     try:
-        sys.path.append(os.getcwd())
-        module_path = os.path.splitext(args.script)[0].replace("/", ".")
-        event_script = importlib.import_module(module_path)
-    except ModuleNotFoundError:
-        eprint(f"ERROR: No event module named '{args.script}'", Palatte.RED)
-        sys.exit(1)
-else:
-    import faker_events.example  # noqa
+        event_generator.start()
+    except KeyboardInterrupt:
+        eprint(f"\nStopping Event Stream.  {event_generator._total_count} in "
+               "total generated.", Palatte.BLUE)
+    finally:
+        if args.update_profiles:
+            eprint(f"Updating the profiles data to {args.profiles_file}",
+                   Palatte.BLUE)
+            profiles_generator.save(args.profiles_file)
 
-try:
-    event_generator.start()
-except KeyboardInterrupt:
-    eprint(f"\nStopping Event Stream.  {event_generator._total_count} in "
-           "total generated.", Palatte.BLUE)
-finally:
-    if args.update_profiles:
-        eprint(f"Updating the profiles data to {args.profiles_file}",
-               Palatte.BLUE)
-        profiles_generator.save(args.profiles_file)
+
+if __name__ == "__main__":
+    sys.exit(cli(parser.parse_args()))
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,63 @@
-# from unittest.mock import patch
+from argparse import Namespace
+from unittest.mock import Mock, patch
+import pytest
 
-# import faker_events
+import faker_events
+from faker_events import __main__ as main
 
-# @patch('faker_events.EventGenerator.start')
-# def test_main(mocked_live_stream):
-#    """
-#    Ensure the live stream is started
-#    """
-#    from faker_events import __main__
-#    mocked_live_stream.assert_called_once()
+
+@pytest.fixture
+def arguments():
+    namespace = Namespace()
+    namespace.num_profiles = 1
+    namespace.profiles_file = None
+    namespace.script = None
+    namespace.update_profiles = False
+    return namespace
+
+
+def setup_function():
+    faker_events.EventGenerator.start = Mock()
+    faker_events.ProfileGenerator.save = Mock()
+
+
+def test_cli_start(arguments):
+    """
+    Ensure the live stream is started
+    """
+    main.cli(arguments)
+    faker_events.EventGenerator.start.assert_called_once()
+
+
+def test_cli_load_script(arguments):
+    """
+    Load a python script if supplied
+    """
+    arguments.script = "application/myscript.py"
+
+    with patch("importlib.import_module") as import_module:
+        main.cli(arguments)
+
+        import_module.assert_called_with("application.myscript")
+
+
+def test_cli_fail_load_script(arguments):
+    """
+    If the supplied python script is not found, return 1 to exit.
+    """
+    arguments.script = "application/myscript.py"
+    patch("importlib.import_module", Mock(side_effect=ModuleNotFoundError()))
+
+    assert main.cli(arguments) == 1
+
+
+def test_cli_update_profiles(capsys, arguments):
+    """
+    Check that the CLI will attempt to run save profiles on completion
+    """
+    arguments.profiles_file = 'profiles.json'
+    arguments.update_profiles = True
+
+    main.cli(arguments)
+
+    faker_events.ProfileGenerator.save.assert_called_with('profiles.json')
